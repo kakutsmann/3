18a08f6e87b7c64da305c1ef6786559f8369c769
kaktusmann <kaktusmann-git@yahoo.de>
2018-04-10 14:54:38 +0200

    Added support for custom cuda paths

--- cuda/make.bash
+++ cuda/make.bash
@@ -2,14 +2,19 @@
 
 go build cuda2go.go || exit 1
 
-NVCC='nvcc --compiler-options -Werror --compiler-options -Wall -Xptxas -O3 -ptx'
+if [ -z ${CUDA_BIN_PATH+x} ] ; then
+    NVCC='nvcc --compiler-options -Werror --compiler-options -Wall -Xptxas -O3 -ptx'
+else
+    NVCC="${CUDA_BIN_PATH}/nvcc --compiler-options -Werror --compiler-options -Wall -Xptxas -O3 -ptx"
+fi
+if [ -z ${CUDA_INC_PATH+x} ] ; then CUDA_INC_PATH="/usr/local/cuda/include"; fi
 
 for f in *.cu; do
 	g=$(echo $f | sed 's/\.cu$//') # file basename
 	for cc in 30 35 37 50 52 53 60 61 70; do
 		if [[ $f -nt $g'_'$cc.ptx ]]; then
 			echo $NVCC -gencode arch=compute_$cc,code=sm_$cc $f -o $g'_'$cc.ptx
-			$NVCC -I/usr/local/cuda/include -gencode arch=compute_$cc,code=sm_$cc $f -o $g'_'$cc.ptx # error can be ignored
+			$NVCC -I"$CUDA_INC_PATH" -gencode arch=compute_$cc,code=sm_$cc $f -o $g'_'$cc.ptx # error can be ignored
 		fi
 	done
 	if [[ $f -nt $g'_wrapper.go' ]]; then
